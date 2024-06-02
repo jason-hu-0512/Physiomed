e669d32198e30c69b6b105ccb82d840c0e9dae7d
Jason Hu <jason@Jasons-MacBook-Pro.local>
2024-06-02 10:35:08 -0400

    first push

--- lib/route_page.dart
+++ lib/route_page.dart
@@ -7,8 +7,31 @@ class RoutePage extends StatefulWidget {
 }
 
 class _RoutePageState extends State<RoutePage> {
+int selected_index = 0;
+static List<Widget>? Widget_options;//the question mark means that the value could be empty we dont know uet
+@override
+void initState() {
+  //we want to override the initstate stopping is a byproduct
+  super.initState(); //this runs the same function as if we didn't override it
+Widget_options=[Home_page(),Injury_page(),Mental_support_page(),Profile_page()];
+}
+
+void onitemtap(int index){
+  setState(() {
+    selected_index=index;
+  });
+}
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Widget_options!.elementAt(selected_index),//choosing from the list above, the home page is 0, and the selected index is what picks values from our "widget options"
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [BottomNavigationBarItem(icon: Icon(Icons.home),label:"Home")],
+      )
+      ,
+    );
   }
 }
--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -10,18 +10,18 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
-  void initState() {
-    super.initState();
+  void initState() {//we want to override the initstate stopping is a byproduct
+    super.initState();//this runs the same function as if we didn't override it
     init();
   }
 
-  Future<void> init() async {
-    await Future.delayed(const Duration(seconds: 2)).then((value) {
-      Navigator.pop(context);
+  Future<void> init() async {//essentially stops the logo screen after two seconds switches to route page
+    await Future.delayed(const Duration(seconds: 2)).then((value) {//waits for two seconds and runs the code inside
+      Navigator.pop(context);//removes the page off the screen
       Navigator.push(
           context,
           MaterialPageRoute(
-              builder: (BuildContext context) => const RoutePage()));
+              builder: (BuildContext context) => const RoutePage()));//goes to the next page
     });
   }
 
